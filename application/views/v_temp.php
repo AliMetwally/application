68e1d868d20d4e6868f12163f953143c39a0bab0
ali <unique.business.sw@gmail.com>
2017-06-20 23:18:35 +0200

    Init Repo

---
+++ application/views/v_temp.php
@@ -0,0 +1,54 @@
+<?php 
+    defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Log in</title>
+
+        <!-- Favicon -->
+        
+        <!-- favicon ends --->
+        
+        <!--- LOAD FILES -->
+        <?php if($_SERVER['HTTP_HOST'] == "localhost" || (stristr($_SERVER['HTTP_HOST'], "192.168.") !== FALSE)|| (stristr($_SERVER['HTTP_HOST'], "127.0.0.") !== FALSE)): ?>
+        <link rel="stylesheet" href="<?=base_url()?>public/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?=base_url()?>public/font-awesome/css/font-awesome.min.css">
+
+        <script src="<?=base_url()?>public/js/jquery.min.js"></script>
+        <script src="<?=base_url()?>public/bootstrap/js/bootstrap.min.js"></script>
+
+        <?php else: ?>
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        <?php endif; ?> 
+        
+        <!-- CSS -->
+        <link rel="stylesheet" href="<?=base_url()?>public/css/form-elements.css">
+        <link rel="stylesheet" href="<?=base_url()?>public/css/style.css">
+        <link rel="stylesheet" href="<?=base_url()?>public/css/main.css">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    </head>
+
+    <body>
+        test
+    </body>
+</html>
---
+++ public/js/main.js
@@ -0,0 +1,41 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var appRoot = setAppRoot('application', 'application');
+
+
+
+
+/************** Plug-ins ****************************** */
+/**
+ * 
+ * @param {type} devFolderName 
+ * @param {type} prodFolderName
+ * @returns {String}
+ */
+function setAppRoot(devFolderName, prodFolderName) {
+    var hostname = window.location.hostname;
+
+    /*
+     * set the appRoot
+     * This will work for both http, https with or without www
+     * @type String
+     */
+
+    //attach trailing slash to both foldernames
+    var devFolder = devFolderName ? devFolderName + "/" : "";
+    var prodFolder = prodFolderName ? prodFolderName + "/" : "";
+
+    var baseURL = "";
+
+    if (hostname.search("localhost") !== -1 || (hostname.search("192.168.") !== -1) || (hostname.search("127.0.0.") !== -1)) {
+        baseURL = window.location.origin + "/" + devFolder;
+    } else {
+        baseURL = window.location.origin + "/" + prodFolder;
+    }
+
+    return baseURL;
+}
+
